1f5bd098258cd5e489ea2814f57a9e839ee7b089
Gautam Kumar <krgautamofficial@gmail.com>
2023-07-05 01:22:42 +0530

    first commit

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,20 @@
+import { Component, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent {
+  @HostListener('window:scroll', ['$event'])
+
+  onWindowScroll() {
+      let element = document.querySelector('.navbar') as HTMLElement;
+      if (window.pageYOffset > element.clientHeight) {
+        element.classList.add('navbar-inverse');
+      } else {
+        element.classList.remove('navbar-inverse');
+      }
+    }
+
+}
